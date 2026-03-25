223cdf010a7c1aa0907f2d227ccd9e462aafbabc
aldulaimi83 <106041458+aldulaimi83@users.noreply.github.com>
2026-03-25 14:19:33 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,10 @@
-const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+export const firebaseConfig = {
+  apiKey: "AIzaSyDUipT-BFyt_h2jMc3UjiHq9G010zP71dg",
+  authDomain: "youooo-chess.firebaseapp.com",
+  databaseURL: "https://youooo-chess-default-rtdb.firebaseio.com/",
+  projectId: "youooo-chess",
+  storageBucket: "youooo-chess.firebasestorage.app",
+  messagingSenderId: "726873828616",
+  appId: "1:726873828616:web:829a600819024bcf2910bd",
+  measurementId: "G-MWL2C57391"
 };
-
-firebase.initializeApp(firebaseConfig);
